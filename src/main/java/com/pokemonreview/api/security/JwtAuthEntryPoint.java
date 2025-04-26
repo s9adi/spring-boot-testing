c18ed67879ba7d87f5ab9dff7dae14a88df85c06
Aditya <saditya009@outlook.com>
2025-04-26 13:13:59 +0530

    JwtAuthEntryPoint added comments

--- src/main/java/com/pokemonreview/api/security/JwtAuthEntryPoint.java
+++ src/main/java/com/pokemonreview/api/security/JwtAuthEntryPoint.java
@@ -11,6 +11,17 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+
+    /**
+     * This method is called when an exception is thrown due to an unauthenticated user trying to access a protected resource.
+     * It sends a 401 Unauthorized response to the client with the exception message.
+     *
+     * @param request       The HttpServletRequest object that contains the request made by the client.
+     * @param response      The HttpServletResponse object that contains the response to be sent to the client.
+     * @param authException The AuthenticationException that was thrown.
+     * @throws IOException      If an input or output error occurs while handling the request or response.
+     * @throws ServletException If a servlet error occurs while handling the request or response.
+     */
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
