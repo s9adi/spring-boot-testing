f5a071502755dc7f44efdf89e089254d75ab27ac
Aditya <saditya009@outlook.com>
2025-04-26 17:32:01 +0530

    Refactored Code

--- src/main/java/com/pokemonreview/api/controllers/PokemonController.java
+++ src/main/java/com/pokemonreview/api/controllers/PokemonController.java
@@ -3,14 +3,11 @@ package com.pokemonreview.api.controllers;
 
 import com.pokemonreview.api.dto.PokemonDto;
 import com.pokemonreview.api.dto.PokemonResponse;
-import com.pokemonreview.api.models.Pokemon;
 import com.pokemonreview.api.service.PokemonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
@@ -18,7 +15,6 @@ public class PokemonController {
 
     private PokemonService pokemonService;
 
-    @Autowired
     public PokemonController(PokemonService pokemonService) {
         this.pokemonService = pokemonService;
     }
